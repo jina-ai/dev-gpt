0f8d04fbbbcc487a9150e0fa46462f578df5ae23
Florian Hönicke <hoenicke.florian@gmail.com>
2023-04-10 01:55:17 +0200

    fix: package

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def read_requirements():
 
 setup(
     name='gptdeploy',
-    version='0.17',
+    version='0.1.7',
     description='Use natural language interface to create, deploy and update your microservice infrastructure.',
     long_description=open('README.md', 'r', encoding='utf-8').read(),
     long_description_content_type='text/markdown',
--- src/server.py
+++ src/server.py
@@ -1,53 +1,53 @@
-from fastapi import FastAPI
-from fastapi.exceptions import RequestValidationError
-from pydantic import BaseModel
-from typing import Optional, Dict
-
-from starlette.middleware.cors import CORSMiddleware
-from starlette.requests import Request
-from starlette.responses import JSONResponse
-from main import main
-
-app = FastAPI()
-
-# Define the request model
-class CreateRequest(BaseModel):
-    test_scenario: str
-    executor_description: str
-
-# Define the response model
-class CreateResponse(BaseModel):
-    result: Dict[str, str]
-    success: bool
-    message: Optional[str]
-
-@app.post("/create", response_model=CreateResponse)
-def create_endpoint(request: CreateRequest):
-
-    result = main(
-        executor_description=request.executor_description,
-        test_scenario=request.test_scenario,
-    )
-    return CreateResponse(result=result, success=True, message=None)
-
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
-# Add a custom exception handler for RequestValidationError
-@app.exception_handler(RequestValidationError)
-def validation_exception_handler(request: Request, exc: RequestValidationError):
-    return JSONResponse(
-        status_code=422,
-        content={"detail": exc.errors()},
-    )
-
-
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run("server:app", host="0.0.0.0", port=8000, log_level="info")
+# from fastapi import FastAPI
+# from fastapi.exceptions import RequestValidationError
+# from pydantic import BaseModel
+# from typing import Optional, Dict
+#
+# from starlette.middleware.cors import CORSMiddleware
+# from starlette.requests import Request
+# from starlette.responses import JSONResponse
+# from main import main
+#
+# app = FastAPI()
+#
+# # Define the request model
+# class CreateRequest(BaseModel):
+#     test_scenario: str
+#     executor_description: str
+#
+# # Define the response model
+# class CreateResponse(BaseModel):
+#     result: Dict[str, str]
+#     success: bool
+#     message: Optional[str]
+#
+# @app.post("/create", response_model=CreateResponse)
+# def create_endpoint(request: CreateRequest):
+#
+#     result = main(
+#         executor_description=request.executor_description,
+#         test_scenario=request.test_scenario,
+#     )
+#     return CreateResponse(result=result, success=True, message=None)
+#
+#
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=["*"],
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
+#
+# # Add a custom exception handler for RequestValidationError
+# @app.exception_handler(RequestValidationError)
+# def validation_exception_handler(request: Request, exc: RequestValidationError):
+#     return JSONResponse(
+#         status_code=422,
+#         content={"detail": exc.errors()},
+#     )
+#
+#
+# if __name__ == "__main__":
+#     import uvicorn
+#     uvicorn.run("server:app", host="0.0.0.0", port=8000, log_level="info")
