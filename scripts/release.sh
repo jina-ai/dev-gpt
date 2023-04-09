8b41c8013570faa5ec5165b8058d27fb2c9f5624
Florian Hönicke <hoenicke.florian@gmail.com>
2023-04-10 01:33:41 +0200

    docs: add examples

--- scripts/release.sh
+++ scripts/release.sh
@@ -76,7 +76,7 @@ if [[ $1 == "final" ]]; then
   printf "this will be a final release: \e[1;33m$RELEASE_VER\e[0m\n"
 
   NEXT_VER=$(echo $RELEASE_VER | awk -F. -v OFS=. 'NF==1{print ++$NF}; NF>1{$NF=sprintf("%0*d", length($NF), ($NF+1)); print}')
-  printf "bump master version to: \e[1;32m$NEXT_VER\e[0m\n"
+  printf "bump main version to: \e[1;32m$NEXT_VER\e[0m\n"
 
 #  make_release_note
 
@@ -92,7 +92,7 @@ elif [[ $1 == 'rc' ]]; then
   DOT_RELEASE_VER=$(echo $RELEASE_VER | sed "s/rc/\./")
   NEXT_VER=$(echo $DOT_RELEASE_VER | awk -F. -v OFS=. 'NF==1{print ++$NF}; NF>1{$NF=sprintf("%0*d", length($NF), ($NF+1)); print}')
   NEXT_VER=$(echo $NEXT_VER | sed "s/\.\([^.]*\)$/rc\1/")
-  printf "bump master version to: \e[1;32m$NEXT_VER\e[0m, this will be the next version\n"
+  printf "bump main version to: \e[1;32m$NEXT_VER\e[0m, this will be the next version\n"
 
 #  make_release_note
 
