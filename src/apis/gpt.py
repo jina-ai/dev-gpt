f22ccc4f9ac1d46abeadbce2e62c2064946f280a
Joschka Braun <joschka.braun@gmail.com>
2023-04-18 16:17:47 +0200

    fix: use old call back for cost calculation

--- src/apis/gpt.py
+++ src/apis/gpt.py
@@ -11,6 +11,8 @@ from openai.error import RateLimitError
 from langchain.schema import AIMessage, HumanMessage, SystemMessage, BaseMessage
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 
+from src.constants import PRICING_GPT4_PROMPT, PRICING_GPT4_GENERATION, PRICING_GPT3_5_TURBO_PROMPT, \
+    PRICING_GPT3_5_TURBO_GENERATION, CHARS_PER_TOKEN
 from src.options.generate.templates_system import template_system_message_base, executor_example, docarray_example, client_example
 from src.utils.string_tools import print_colored
 
@@ -20,7 +22,19 @@ class GPTSession:
         self.task_description = task_description
         self.test_description = test_description
         self.configure_openai_api_key()
-        self.model_name = 'gpt-4' if model == 'gpt-4' and self.is_gpt4_available() else 'gpt-3.5-turbo'
+        if model == 'gpt-4' and self.is_gpt4_available():
+            self.model_name = 'gpt-4'
+            self.pricing_prompt = PRICING_GPT4_PROMPT
+            self.pricing_generation = PRICING_GPT4_GENERATION
+        else:
+            if model == 'gpt-4':
+                print_colored('GPT-4 is not available. Using GPT-3.5-turbo instead.', 'yellow')
+                model = 'gpt-3.5-turbo'
+            self.model_name = model
+            self.pricing_prompt = PRICING_GPT3_5_TURBO_PROMPT
+            self.pricing_generation = PRICING_GPT3_5_TURBO_GENERATION
+        self.chars_prompt_so_far = 0
+        self.chars_generation_so_far = 0
 
     @staticmethod
     def configure_openai_api_key():
@@ -50,11 +64,22 @@ If you have updated it already, please restart your terminal.
                     continue
             return True
         except openai.error.InvalidRequestError:
-            print_colored('GPT-4 is not available. Using GPT-3.5-turbo instead.', 'yellow')
             return False
 
+    def cost_callback(self, chars_prompt, chars_generation):
+        self.chars_prompt_so_far += chars_prompt
+        self.chars_generation_so_far += chars_generation
+        print('\n')
+        money_prompt = self.calculate_money_spent(self.chars_prompt_so_far, self.pricing_prompt)
+        money_generation = self.calculate_money_spent(self.chars_generation_so_far, self.pricing_generation)
+        print('Total money spent so far on openai.com:', f'${money_prompt + money_generation:.3f}')
+        print('\n')
+
+    def calculate_money_spent(self, num_chars, price):
+        return round(num_chars / CHARS_PER_TOKEN * price / 1000, 3)
+
     def get_conversation(self, system_definition_examples: List[str] = ['executor', 'docarray', 'client']):
-        return _GPTConversation(self.model_name, self.task_description, self.test_description, system_definition_examples)
+        return _GPTConversation(self.model_name, self.cost_callback, self.task_description, self.test_description, system_definition_examples)
 
 
 class AssistantStreamingStdOutCallbackHandler(StreamingStdOutCallbackHandler):
@@ -64,7 +89,7 @@ class AssistantStreamingStdOutCallbackHandler(StreamingStdOutCallbackHandler):
 
 
 class _GPTConversation:
-    def __init__(self, model: str, task_description, test_description, system_definition_examples: List[str] = ['executor', 'docarray', 'client']):
+    def __init__(self, model: str, cost_callback, task_description, test_description, system_definition_examples: List[str] = ['executor', 'docarray', 'client']):
         self._chat = ChatOpenAI(
             model_name=model,
             streaming=True,
@@ -72,6 +97,7 @@ class _GPTConversation:
             verbose=os.environ['VERBOSE'].lower() == 'true',
             temperature=0,
         )
+        self.cost_callback = cost_callback
         self.messages: List[BaseMessage] = []
         self.system_message = self._create_system_message(task_description, test_description, system_definition_examples)
         if os.environ['VERBOSE'].lower() == 'true':
@@ -83,9 +109,11 @@ class _GPTConversation:
         if os.environ['VERBOSE'].lower() == 'true':
             print_colored('user', prompt, 'blue')
             print_colored('assistant', '', 'green', end='')
-        response = self._chat([self.system_message] + self.messages)
+        messages = [self.system_message] + self.messages
+        response = self._chat(messages)
         if os.environ['VERBOSE'].lower() == 'true':
             print()
+        self.cost_callback(sum([len(m.content) for m in messages]), len(response.content))
         self.messages.append(response)
         return response.content
 
