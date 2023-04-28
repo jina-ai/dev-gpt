aab91a4077c22aaa38713f6c3f66f9a458b4fc4f
Florian Hönicke <hoenicke.florian@gmail.com>
2023-04-28 23:17:51 +0200

    🧪 test: level 1

--- test/test_generator.py
+++ test/test_generator.py
@@ -1,7 +1,30 @@
 import os
 from src.options.generate.generator import Generator
 
-def test_generator(tmpdir):
+# The cognitive difficulty level is determined by the number of Requirements the microservice has.
+
+def test_generation_level_0(tmpdir):
+    """
+    Requirements:
+    pip packages: ❌
+    environment: ❌
+    GPT-3.5-turbo: ❌
+    APIs: ❌
+    Databases: ❌
+    """
     os.environ['VERBOSE'] = 'true'
-    generator = Generator("The microservice is very simple, it does not take anything as input and only outputs the word 'test'", "my test", str(tmpdir) + 'microservice', 'gpt-3.5-turbo')
+    generator = Generator("The microservice is very simple, it does not take anything as input and only outputs the word 'test'", str(tmpdir) + 'microservice', 'gpt-3.5-turbo')
+    generator.generate()
+
+def test_generation_level_1(tmpdir):
+    """
+    Requirements:
+    pip packages: ✅
+    environment: ❌
+    GPT-3.5-turbo: ❌
+    APIs: ❌
+    Databases: ❌
+    """
+    os.environ['VERBOSE'] = 'true'
+    generator = Generator("The input is a PDF like https://www.africau.edu/images/default/sample.pdf and the output the parsed text", str(tmpdir) + 'microservice', 'gpt-3.5-turbo')
     generator.generate()
