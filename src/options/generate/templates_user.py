ea5a4dd9b7568114dc0af628508bee5e83befd31
Florian Hönicke <hoenicke.florian@gmail.com>
2023-05-04 11:44:44 +0200

    🗄 fix: dont output other files

--- src/options/generate/templates_user.py
+++ src/options/generate/templates_user.py
@@ -280,18 +280,14 @@ Output the apt-get packages that need to be placed at {{apt_get_packages}} as js
 ```json
 {{"packages": ["<package1>", "<package2>"]}}
 ```
-Example for the following requirements.txt file:
-**requirements.txt**
-```
-numpy==1.19.5
-fitz
-```
-The output would be:
+Example:
+Error is about missing package `libgl1-mesa-glx`.
+The output is:
 **apt-get-packages.json**
 ```json
-{{"packages": []}}
+{{"packages": [libgl1-mesa-glx]}}
 ```
-Note that you must not output any other files. Only output the apt-get-packages.json file.
+Note that you must not output the content of any other files. Only output the apt-get-packages.json file.
 '''
 )
 
