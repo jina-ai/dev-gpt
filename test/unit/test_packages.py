8e65d0fc4203f217bfbb8ace4df3f929d56294fd
Florian Hönicke <hoenicke.florian@gmail.com>
2023-05-27 12:08:27 +0200

    ➕ refactor: summarize error message without line number

--- test/unit/test_packages.py
+++ test/unit/test_packages.py
@@ -1,16 +1,15 @@
 from dev_gpt.options.generate.generator import Generator
 
 def test_package_parsing():
-    packages_json_string = '''\
-    [
+    packages_json = [
       ["PyPDF2", "gpt_3_5_turbo"],
       ["pdfminer.six", "gpt_3_5_turbo"],
       ["tika", "gpt_3_5_turbo"],
       [],
       ["gpt_3_5_turbo"]
-    ]'''
+    ]
 
-    parsed_packages = Generator.process_packages_json_string(packages_json_string)
+    parsed_packages = Generator.process_packages_json_string(packages_json)
     for parsed, expected in zip(parsed_packages, [
         ['pypdf2', 'gpt_3_5_turbo'],
         ['pdfminer.six', 'gpt_3_5_turbo'],
